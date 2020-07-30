0e119126796e77eb34ec27a01bff0e6cf03d4140
Dan Waxman <dan.waxman1@gmail.com>
2020-07-30 00:01:44 -0400

    Parallelized

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import random
-from multiprocessing import Pool
-
+import multiprocessing as mp
 import numpy as np
 from PIL import Image
 
@@ -43,20 +42,19 @@ class Mandelbrot:
 
 
 class Buddhabrot:
-    def __init__(self, height, width, points=10000, max_it=1000, escape_z=4, x_bounds=(-2,2), y_bounds=(-2,2)):
+    def __init__(self, height, width, points=10000, max_it=1000, x_bounds=(-2,1.5), y_bounds=(-1.3,1.3)):
         self.height=height
         self.width=width
         self.points=points
         self.max_it=max_it
-        self.escape_z=escape_z
         self.x_bounds = x_bounds
         self.y_bounds = y_bounds
 
         self.image_data = np.zeros((self.height, self.width, 3), dtype=np.float64)
 
     def c2index(self, c):
-        x = int((np.real(c) - self.x_bounds[0]) * self.width / (abs(self.x_bounds[0]) + abs(self.x_bounds[1])))
-        y = int((np.imag(c) - self.y_bounds[0]) * self.height / (abs(self.y_bounds[0]) + abs(self.y_bounds[1])))
+        x = int((c.real - self.x_bounds[0]) * self.width / (abs(self.x_bounds[0]) + abs(self.x_bounds[1])))
+        y = int((c.imag - self.y_bounds[0]) * self.height / (abs(self.y_bounds[0]) + abs(self.y_bounds[1])))
 
         return (x, y)
 
@@ -67,25 +65,29 @@ class Buddhabrot:
         for i in range(self.max_it):
             z = z**2 + c
             trace.append(z)
-            if np.abs(z) > self.escape_z:
+            if z.real*z.real + z.imag*z.imag > 4.0:
                 return trace
 
         return []
 
 
-    def generate_image(self):
-        for n in range(self.points):
-            if n % 100000 == 0:
-                print(n)
-            rand_c = random.uniform(-2, 2) + random.uniform(-2, 2) * 1j
+    def thread_generate(self, points):
+        tmp = np.zeros((self.height, self.width, 3), dtype=np.float64)
+        for n in range(points):
+            rand_c = random.uniform(*self.x_bounds) + random.uniform(*self.y_bounds) * 1j
 
             result = self.iterate_point(rand_c)
             for trace_z in result:
                 x, y = self.c2index(trace_z)
 
                 if 0 <= x < self.width and 0 <= y < self.height:
-                    self.image_data[y, x] = self.image_data[y, x] + 1
+                    tmp[y, x] = tmp[y, x] + 1
 
+        return tmp
+
+    def generate_image(self):
+        pool = mp.Pool(12)
+        self.image_data = np.sum(pool.map(self.thread_generate, iter([self.points // 12] * 12)), axis=0)
         self.image_data = self.image_data / np.max(self.image_data) * 255
 
     def get_image(self):
@@ -95,20 +97,18 @@ class Buddhabrot:
         return self.image_data
 
 if __name__ == '__main__':
-    red_img = Buddhabrot(1000, 1000, max_it=5000, points=10000000)
+    red_img = Buddhabrot(500, 673, max_it=100, points=10000000)
     red_img.generate_image()
     red_img_array = red_img.get_image_array()
 
-    green_img = Buddhabrot(1000, 1000, max_it=500, points=10000000)
+    green_img = Buddhabrot(500, 673, max_it=50, points=10000000)
     green_img.generate_image()
     green_img_array = green_img.get_image_array()
 
-    blue_img = Buddhabrot(1000, 1000, max_it=50, points=10000000)
+    blue_img = Buddhabrot(500, 673, max_it=100, points=10000000)
     blue_img.generate_image()
     blue_img_array = blue_img.get_image_array()
 
     img_array = np.stack([red_img_array[:,:,0], green_img_array[:,:,0], blue_img_array[:,:,0]], axis=2)
-
     img = Image.fromarray(img_array.astype(np.uint8), 'RGB')
-
     img.show()
